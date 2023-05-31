2d06b306f7656b1ce83fa3fd840804eb36a7909d
Ambition <xuzhi1994@hotmail.com>
2023-05-31 15:08:53 +0800

    add comment to 279 solution

--- dynamic_programming/279_Perfect_Squares.py
+++ dynamic_programming/279_Perfect_Squares.py
@@ -1,4 +1,11 @@
 # use dynamic programming way to solve this problem
+# 1. init dp list is important, cause 1 was the smallest number for square number,
+#    so dp[i] at most equal to i
+# 2. the transition formula in the inner loop of two loops,
+#    dp[i] equal to dp[i-k] plus 1, which k was a square number less than i
+# But due to two for loop, time complexity is high, which is O(n*sqrt(n))
+
+
 class Solution:
     def numSquares(self, n: int) -> int:
         dp = [i for i in range(n + 1)]
